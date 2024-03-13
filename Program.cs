3c4dc1e3e6416ed7e27a25a6b5109f0960aa46fb
Chandra Ardiansyah <chandraardiansyah265@gmail.com>
2024-03-13 11:05:22 +0700

    day 1

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Struktur Dasar Di C#
+//
+// using System;
+// namespace SkilvulLearning
+// {
+//     class Animal
+//     {
+//         public static void Main(string[] args)
+//         {
+//             Console.WriteLine("Animal List");
+//             Console.WriteLine("-----------");
+//             Console.WriteLine("1. Chciken");
+//             Console.WriteLine("2. Duck");
+//         }
+//     }
+// }
+
+//Beberapa Sintaks Di C#
+
+//Sintaks Print
+// Console.WriteLine("Hello");
+// Console.WriteLine("Hello" + "Hai");
+// Console.WriteLine(7);
+// Console.WriteLine(7.5);
+// Console.WriteLine("November " + 4);
+// Console.WriteLine(11 + 4);
+
+using System;
+namespace SkilvulLearning
+{
+    class Variable{
+        public static void Main(string[] args)
+        {
+            //Deklarsi Variable Eksplisit
+            // string nama;
+            // int umur;
+            // Float beratBadan;
+            // bool isMenikah;
+
+            //Inisialisasi Variable
+            // nama = "Chandra";
+            // umur = 18;
+            // beratBadan = 49.5;
+            // isMenikah = false;
+
+            //Deklarasi dan Inisialisasi secara langsung
+            // string nama = "Chandra";
+            // int umur = 18;
+            // Console.WriteLine(nama + " berumur " + umur + " tahun");
+
+            //Deklarasi Variable Implisit
+            // var nama = "Chandra";
+        }
+    }
+}
